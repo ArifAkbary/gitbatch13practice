8643a44bce001cab17ba58c2b35caea6ab20f6bb
ArifAkbary <arifakbary022@gmail.com>
2022-08-24 17:03:33 -0700

    temperature check basic code is there in the repo

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,17 @@
+package class01;
+
+public class gitBasics {
+
+
+    public static void main(String[] args) {
+
+       //write a code and tell me that to day is beutifull day
+        // and print in the concel
+
+
+        System.out.println("today is a beutifull day");
+
+
+    }
+
+}
